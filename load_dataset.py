c231e004727594093ab116c7f01864589c428577
Chris Allum <ctallum@gmail.com>
2021-10-04 22:17:17 -0400

    start working on loading funcs

---
+++ load_dataset.py
@@ -0,0 +1,19 @@
+def load_train_set():
+    '''
+    load_train_set: Return training data set for Rock Paper Scisors ML model
+    
+    Args: NONE
+    Returns: np array []
+
+    '''
+    pass
+
+def load_test_set():
+    '''
+    load_test_set: Return test data set for Rock Paper Scisors ML model
+    
+    Args: NONE
+    Returns: np array []
+
+    '''
+    pass
